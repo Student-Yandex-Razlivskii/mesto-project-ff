8b838685b2a10810589713a788da843bf67f308d
Andrei Razlivskii <razlivskii@yandex.ru>
2024-02-13 22:10:51 +0300

    1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+const card = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
-// @todo: DOM узлы
 
-// @todo: Функция создания карточки
 
-// @todo: Функция удаления карточки
+function getCard (item, deleteCard){
+  const cardElement = card.querySelector('.card').cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  cardImage.src = item.link;
+  cardImage.alt = item.name;
+  cardTitle.textContent = item.name;
 
-// @todo: Вывести карточки на страницу
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  
+  deleteButton.addEventListener('click', function() {
+    deleteCard(cardElement);
+  })
+  
+  return cardElement;
+}
+
+function addCard(cardData) {
+  cardList.append(getCard(cardData, deleteCard))
+  
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+initialCards.forEach((function(element) {
+  addCard(element);
+}))
+
